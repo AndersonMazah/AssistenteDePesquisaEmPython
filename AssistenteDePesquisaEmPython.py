c51aa77a446a492163469ace708c9722ca648296
Anderson Martins <anderson.mazah@outlook.com>
2022-10-23 23:50:48 -0300

    add file

---
+++ AssistenteDePesquisaEmPython.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import os
+import webbrowser
+
+#Função para ouvir e reconhecer a fala
+def ouvir_microfone():
+    #Habilita o microfone do usuário
+    microfone = sr.Recognizer()
+    
+    #usando o microfone
+    with sr.Microphone() as source:
+        
+        #Chama um algoritmo de reducao de ruidos no som
+        microfone.adjust_for_ambient_noise(source)
+        
+        #Frase para o usuario dizer algo
+        print("Diga alguma coisa: ")
+        
+        #Armazena o que foi dito numa variavel
+        audio = microfone.listen(source)
+        
+    try:
+        
+        #Passa a variável para o algoritmo reconhecedor de padroes
+        frase = microfone.recognize_google(audio,language='pt-BR')
+
+        if "encontrar" in frase:
+            url =  "http://maps.google.com/maps?q=" + frase
+        
+        elif "pesquisar" in frase:
+            url =  "http://google.com/search?q=" + frase
+        
+        webbrowser.get().open(url)
+        #Retorna a frase pronunciada
+        #print("Você disse: " + frase)
+        
+    #Se nao reconheceu o padrao de fala, exibe a mensagem
+    except sr.UnkownValueError:
+        print("Não entendi")
+        
+    return frase
+
+ouvir_microfone()
